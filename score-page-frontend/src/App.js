83a75bbdbee7a702d5a991a45a54976215e08d09
Adam Holbrook <adam@futrli.com>
2020-01-03 13:21:11 +0000

    core page with first request

--- score-page-frontend/src/App.js
+++ score-page-frontend/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Teams from './components/teams';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      teams: [],
+};
+
+}
+  componentDidMount() {
+    fetch('https://www.api-football.com/demo/api/v2/teams/team/24')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ teams: data.api.teams })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    console.log(this.state);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Team scores and details page!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div id='form-holder'>
+        <div id='team-input'>
+          <label for="name" id='name-label'>Team Name:</label>
+          <input type="text" id="team-name" name="team-name" placeholder="Enter your team name here"></input>
+        </div>
+        <div id='team-details'>
+        <Teams teams={this.state.teams}/>
+        </div>
+      </div>
     </div>
   );
-}
+}}
 
 export default App;
